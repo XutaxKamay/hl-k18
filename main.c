d72b81356a228057ec8229b7c7f867a165505b1e
kamay <xutaxkamay@gmail.com>
2020-07-20 01:19:23 +0200

    Added some flags for port bits

--- main.c
+++ main.c
@@ -17,28 +17,13 @@ void delay(unsigned long delay)
 
 void main(void)
 {
+    /**
+     * Set all to output for now
+     */
     TRISA = 0b00000000;
     TRISB = 0b00000000;
     TRISC = 0b00000000;
     TRISD = 0b00000000;
     TRISE = 0b00000000;
-
-    PORTA = 0b00000000;
-    PORTB = 0b00000000;
-    PORTC = 0b00000000;
-    PORTD = 0b00000000;
-    PORTE = 0b00000000;
-
-    while (1)
-    {
-        // ? ? ? col4 ? col3 col2 col1 ?
-        PORTA = 0b00101110;
-        // l 8,7,6,5,4,3,2,1
-        PORTD = 0b11111111;
-        // ? ? ? ? ? ? speaker col8
-        PORTC = 0b00000001;
-        // ? ? ? ? ? col7 col6 col5
-        PORTE = 0b00000111;
-    }
 }
 
